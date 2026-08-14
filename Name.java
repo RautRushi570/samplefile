93bef57cf53138352ca68114a45576c10e39492c
Sam <sam678@gmail.com>
2026-08-14 12:20:46 +0530

    Removed the msg

--- Name.java
+++ Name.java
@@ -6,7 +6,5 @@ public class Name{
 		System.out.println("Hello Rushikesh!!");
 		System.out.println("How are you?");
 		System.out.println("All good?");
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter a number : ");
 	}
 }
