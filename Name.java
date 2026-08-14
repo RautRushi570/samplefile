e2da2b228a9f45d0ee47123de99365122dd0114c
Sam <sam678@gmail.com>
2026-08-14 12:13:38 +0530

    Added a new msg

--- Name.java
+++ Name.java
@@ -1,7 +1,12 @@
+import java.util.*;
+
+
 public class Name{
 	public static void main(String[] args){
 		System.out.println("Hello Rushikesh!!");
 		System.out.println("How are you?");
 		System.out.println("All good?");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a number : ");
 	}
 }
